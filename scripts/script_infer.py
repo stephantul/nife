fd9d8d447925f3e0ee6f325caec86086d24b92b7
stephantul <stephantul@gmail.com>
2025-09-29 20:46:44 +0200

    fix infer script

--- scripts/script_infer.py
+++ scripts/script_infer.py
@@ -14,8 +14,15 @@ if __name__ == "__main__":
     model_name = "mixedbread-ai/mxbai-embed-large-v1"
     model = SentenceTransformer(model_name)
 
-    paths = ["sample/10BT/000_00000.parquet", "sample/10BT/000_00001.parquet", "sample/10BT/000_00002.parquet"]
+    paths = [
+        "sample/10BT/003_00000.parquet",
+        "sample/10BT/004_00000.parquet",
+        "sample/10BT/005_00000.parquet",
+        "sample/10BT/006_00000.parquet",
+        "sample/10BT/007_00000.parquet",
+        "sample/10BT/008_00000.parquet",
+    ]
     for path in paths:
         data = load_dataset("HuggingFaceFW/fineweb", "sample-10BT", streaming=True, split="train", data_files=path)
         texts = (item["text"] for item in data)
-        infer(model, texts, batch_size=128, name=f"output/{model_name.replace('/', '_')}", save_every=16)
+        infer(model, texts, batch_size=512, name=f"output/{path.replace('/', '_')}", save_every=16)
