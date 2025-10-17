0bee775ccf20f2b943f3f5b01ee7c9cf2e793338
stephantul <stephantul@gmail.com>
2025-10-17 20:31:43 +0200

    remove id stuff

--- pystatic/distillation/infer.py
+++ pystatic/distillation/infer.py
@@ -41,11 +41,10 @@ def _write_data(path: Path, pooled: list[torch.Tensor], records: list[dict[str, 
     pooled_tensor = torch.cat(pooled, dim=0).float()
     torch.save(pooled_tensor, path / f"pooled_{shard_index:04d}.pt")
     with open(path / f"texts_{shard_index:04d}.txt", "w", encoding="utf-8") as f:
-        for i, record in enumerate(records):
-            other_keys = {k for k in record if k not in {"text", "id", "index"}}
-            dict_without_other_keys = {k: v for k, v in record.items() if k in other_keys}
+        for record in records:
+            record["text"] = record.pop("truncated")
             line = json.dumps(
-                {"text": record["truncated"], "id": record["id"], "index": i, **dict_without_other_keys},
+                record,
                 ensure_ascii=False,
             )
             f.write(line + "\n")
