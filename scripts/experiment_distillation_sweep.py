2228cebc0b861b81b7d0eb245b800f0b95ec284b
stephantul <stephantul@gmail.com>
2025-10-21 09:47:11 +0200

    fix sweep

--- scripts/experiment_distillation_sweep.py
+++ scripts/experiment_distillation_sweep.py
@@ -25,13 +25,13 @@ def _parse_args() -> argparse.Namespace:
     parser.add_argument("--model-dim", type=int, default=1024, help="Dimensionality of the model.")
     parser.add_argument("--batch-size", type=int, default=2048, help="Batch size for training.")
     parser.add_argument("--epochs", type=int, default=5, help="Number of epochs for training.")
-    parser.add_argument("--total-steps", type=int, help="Total number of training steps, -1 for automatic.")
     parser.add_argument("--learning-rate", type=float, default=0.05, help="Learning rate for training.")
     parser.add_argument(
         "--tokenizer-path", type=str, default="mixedbread-ai/mxbai-embed-large-v1", help="Path to the tokenizer."
     )
     parser.add_argument("--limit-shards", type=int, help="Limit the number of shards.")
     parser.add_argument("--in-memory", action="store_true", help="Load the dataset in memory.")
+    parser.add_argument("--initialize-from-model", type=str, help="Path to a model to initialize from.")
     return parser.parse_args()
 
 
@@ -39,23 +39,20 @@ if __name__ == "__main__":
     parsed_args = _parse_args()
     model_dim = parsed_args.model_dim
 
-    initialize_from_model = ["mixedbread-ai/mxbai-embed-large-v1"]
-    use_matryoshka = [True, False]
+    dense = [True, False]
 
-    for model_to_initialize_from, mat in product(initialize_from_model, use_matryoshka):
+    for (with_dense,) in product(dense):
         experiment_name_parts = [
             parsed_args.name,
-            f"init_{'none' if model_to_initialize_from is None else 'from_model'}",
-            f"matryoshka_{mat}",
+            f"dense_{with_dense}",
         ]
         parsed_args.experiment_name = "_".join(experiment_name_parts)
 
         model = initialize_model(
             tokenizer_path=parsed_args.tokenizer_path,
-            model_to_initialize_from=model_to_initialize_from,
+            model_to_initialize_from=parsed_args.initialize_from_model,
             model_dim=model_dim,
-            with_norm=False,
-            with_weights=False,
+            with_dense=with_dense,
         )
         dataset, n_samples = load_data(
             train_datasets=parsed_args.train_dataset,
@@ -71,5 +68,5 @@ if __name__ == "__main__":
             parsed_args.learning_rate,
             parsed_args.epochs,
             l2_norm=None,
-            use_matryoshka=mat,
+            use_matryoshka=True,
         )
